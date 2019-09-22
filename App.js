e92292316b18a12a05e7df94b0e9027690340f36
Guilherme Nascimento <gui.heavy@gmail.com>
2019-09-22 16:02:29 -0300

    Add a context creator.

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import React from 'react';
-import { BlogProvider } from './src/context/BlogContext';
+import { Provider } from './src/context/BlogContext';
 
 const navigator = createStackNavigator(
   {
@@ -18,8 +18,8 @@ const navigator = createStackNavigator(
 const App = createAppContainer(navigator);
 export default () => {
   return (
-    <BlogProvider>
+    <Provider>
       <App />
-    </BlogProvider>
+    </Provider>
   );
 };
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,23 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import { Context } from '../context/BlogContext';
+
 const HomeScreen = () => {
-return(
-<View><Text>HomeScreen</Text></View>
-);
-}
+  const { state, addBlogPost } = useContext(Context);
+
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Button title='Add Post' onPress={addBlogPost}></Button>
+      <FlatList
+        data={state}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
+    </View>
+  );
+};
 const styles = StyleSheet.create({});
 export default HomeScreen;
